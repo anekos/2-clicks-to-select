e6043e2741005d8601f9bab4d4c4a3b73d971484
anekos <anekos@snca.net>
2021-09-26 12:06:26 +0900

    Oops

--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,54 @@ const webpack = require('webpack')
 const TerserPlugin = require('terser-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-  entry: {
-    main: './src/main.tsx',
-    background: './src/background.js',
-    options: './src/options.tsx',
-  },
-  output: {
-    path: __dirname + '/dist',
-    clean: true
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false, // LICENCE.txtを生成しない
-      }),
-    ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
+
+module.exports = (env, options) => {
+  return {
+    entry: {
+      main: './src/main.tsx',
+      background: './src/background.js',
+      options: './src/options.tsx',
+    },
+    output: {
+      path: __dirname + '/dist',
+      clean: true
+    },
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+    },
+    module: {
+      rules: [
         {
-          from: process.env.NODE_ENV === 'development' ? './manifest.dev.json' : './manifest.json',
-          to: 'manifest.json'
-        },
-        {
-          from: './icon/*.*',
-          to: './',
-        },
-        {
-          from: './src/options.html',
-          to: 'options.html',
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
         },
       ],
-    }),
-  ],
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false, // LICENCE.txtを生成しない
+        }),
+      ],
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          {
+            from: options.mode === 'development' ? './manifest.dev.json' : './manifest.json',
+            to: 'manifest.json'
+          },
+          {
+            from: './icon/*.*',
+            to: './',
+          },
+          {
+            from: './src/options.html',
+            to: 'options.html',
+          },
+        ],
+      }),
+    ],
+  }
 }
